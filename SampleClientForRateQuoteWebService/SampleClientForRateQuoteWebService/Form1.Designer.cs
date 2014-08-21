955c7f3f908673313d9363f24b008b155b954ab5
RobertVignato <rob.vig@gmail.com>
2014-08-21 14:30:01 -0400

    initial upload
    Here's the code from GlobalTranz by Baljinder

---
+++ SampleClientForRateQuoteWebService/SampleClientForRateQuoteWebService/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace SampleClientForRateQuoteWebService
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnRateMyShipment = new System.Windows.Forms.Button();
+			this.dgvRateMyShipment = new System.Windows.Forms.DataGridView();
+			this.textBoxBolNo = new System.Windows.Forms.TextBox();
+			this.btnConfirmShipment = new System.Windows.Forms.Button();
+			this.txtIndex = new System.Windows.Forms.TextBox();
+			this.label32 = new System.Windows.Forms.Label();
+			this.btnMyShipmentTrack = new System.Windows.Forms.Button();
+			this.dgvShipmentTrack = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.dgvRateMyShipment)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dgvShipmentTrack)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btnRateMyShipment
+			// 
+			this.btnRateMyShipment.Location = new System.Drawing.Point(13, 13);
+			this.btnRateMyShipment.Name = "btnRateMyShipment";
+			this.btnRateMyShipment.Size = new System.Drawing.Size(144, 23);
+			this.btnRateMyShipment.TabIndex = 0;
+			this.btnRateMyShipment.Text = "1. Rate My Shipment";
+			this.btnRateMyShipment.UseVisualStyleBackColor = true;
+			this.btnRateMyShipment.Click += new System.EventHandler(this.btnRateMyShipment_Click);
+			// 
+			// dgvRateMyShipment
+			// 
+			this.dgvRateMyShipment.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvRateMyShipment.Location = new System.Drawing.Point(13, 42);
+			this.dgvRateMyShipment.Name = "dgvRateMyShipment";
+			this.dgvRateMyShipment.Size = new System.Drawing.Size(913, 133);
+			this.dgvRateMyShipment.TabIndex = 1;
+			// 
+			// textBoxBolNo
+			// 
+			this.textBoxBolNo.Location = new System.Drawing.Point(162, 208);
+			this.textBoxBolNo.Name = "textBoxBolNo";
+			this.textBoxBolNo.ReadOnly = true;
+			this.textBoxBolNo.Size = new System.Drawing.Size(80, 20);
+			this.textBoxBolNo.TabIndex = 36;
+			// 
+			// btnConfirmShipment
+			// 
+			this.btnConfirmShipment.Location = new System.Drawing.Point(12, 207);
+			this.btnConfirmShipment.Name = "btnConfirmShipment";
+			this.btnConfirmShipment.Size = new System.Drawing.Size(144, 20);
+			this.btnConfirmShipment.TabIndex = 35;
+			this.btnConfirmShipment.Text = "2. Confirm Shipment";
+			this.btnConfirmShipment.UseVisualStyleBackColor = true;
+			this.btnConfirmShipment.Click += new System.EventHandler(this.btnConfirmShipment_Click);
+			// 
+			// txtIndex
+			// 
+			this.txtIndex.Location = new System.Drawing.Point(367, 181);
+			this.txtIndex.Name = "txtIndex";
+			this.txtIndex.Size = new System.Drawing.Size(61, 20);
+			this.txtIndex.TabIndex = 38;
+			// 
+			// label32
+			// 
+			this.label32.AutoSize = true;
+			this.label32.Location = new System.Drawing.Point(10, 184);
+			this.label32.Name = "label32";
+			this.label32.Size = new System.Drawing.Size(351, 13);
+			this.label32.TabIndex = 37;
+			this.label32.Text = "Please select a carrier index from the grid below after Rate My Shipment :";
+			// 
+			// btnMyShipmentTrack
+			// 
+			this.btnMyShipmentTrack.Location = new System.Drawing.Point(12, 233);
+			this.btnMyShipmentTrack.Name = "btnMyShipmentTrack";
+			this.btnMyShipmentTrack.Size = new System.Drawing.Size(144, 20);
+			this.btnMyShipmentTrack.TabIndex = 39;
+			this.btnMyShipmentTrack.Text = "3. My Shipment Track";
+			this.btnMyShipmentTrack.UseVisualStyleBackColor = true;
+			this.btnMyShipmentTrack.Click += new System.EventHandler(this.btnMyShipmentTrack_Click);
+			// 
+			// dgvShipmentTrack
+			// 
+			this.dgvShipmentTrack.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvShipmentTrack.Location = new System.Drawing.Point(12, 259);
+			this.dgvShipmentTrack.Name = "dgvShipmentTrack";
+			this.dgvShipmentTrack.Size = new System.Drawing.Size(913, 95);
+			this.dgvShipmentTrack.TabIndex = 40;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(938, 368);
+			this.Controls.Add(this.dgvShipmentTrack);
+			this.Controls.Add(this.btnMyShipmentTrack);
+			this.Controls.Add(this.txtIndex);
+			this.Controls.Add(this.label32);
+			this.Controls.Add(this.textBoxBolNo);
+			this.Controls.Add(this.btnConfirmShipment);
+			this.Controls.Add(this.dgvRateMyShipment);
+			this.Controls.Add(this.btnRateMyShipment);
+			this.Name = "Form1";
+			this.Text = "Sample Client for Rate Quote Web Service";
+			((System.ComponentModel.ISupportInitialize)(this.dgvRateMyShipment)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dgvShipmentTrack)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnRateMyShipment;
+		private System.Windows.Forms.DataGridView dgvRateMyShipment;
+		private System.Windows.Forms.TextBox textBoxBolNo;
+		private System.Windows.Forms.Button btnConfirmShipment;
+		private System.Windows.Forms.TextBox txtIndex;
+		private System.Windows.Forms.Label label32;
+		private System.Windows.Forms.Button btnMyShipmentTrack;
+		private System.Windows.Forms.DataGridView dgvShipmentTrack;
+	}
+}
+
---
+++ SampleClientForRateQuoteWebService/SampleClientForRateQuoteWebService/SampleClientForRateQuoteWebService/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Windows.Forms;
+using SampleClientForRateQuoteWebService.RateQuoteWebServiceReference;
+using System.Reflection;
+using System.Data;
+namespace SampleClientForRateQuoteWebService
+{
+	public partial class Form1 : Form
+	{
+		/// <summary>
+		/// The username.
+		/// </summary>
+		private string Username = "brownjordan6";
+
+		/// <summary>
+		/// The password.
+		/// </summary>
+		private string Password = "Thursday%27";
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Form1"/> class.
+		/// </summary>
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Handles the Click event of the btnRateMyShipment control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+		private void btnRateMyShipment_Click(object sender, EventArgs e)
+		{
+			QuoteInfo quoteInfo = new QuoteInfo();
+			QuoteItemsInfo[] quoteItemsInfo;
+
+			FillQuoteDetails(out quoteInfo, out quoteItemsInfo);
+
+			RateQuoteWebServiceReference.Service rateQuoteWebService = new RateQuoteWebServiceReference.Service();
+			QuoteRates[] quoteRates = null;
+			quoteRates = rateQuoteWebService.RateMyShipment(quoteInfo, quoteItemsInfo);
+			if (quoteRates != null)
+			{
+				dgvRateMyShipment.DataSource = quoteRates;
+			}
+			else
+			{
+				dgvRateMyShipment.DataSource = null;
+				MessageBox.Show("No carriers Found");
+			}
+		}
+
+		/// <summary>
+		/// Fills the quote details.
+		/// </summary>
+		/// <param name="quoteInfo">The quote information.</param>
+		/// <param name="quoteItemsInfo">The quote items information.</param>
+		private void FillQuoteDetails(out QuoteInfo quoteInfo, out QuoteItemsInfo[] quoteItemsInfo)
+		{
+			// Quote Info.
+			quoteInfo = new QuoteInfo();
+			quoteInfo.ShipmentOriginZip = "98001";
+			quoteInfo.ShipmentDestinationZip = "10001";
+			quoteInfo.ShipmentRequestDate = DateTime.Today;
+			quoteInfo.ShipperUserNameWithCarrierRate = Username;
+			quoteInfo.ShipperPasswordWithCarrierRate = Password;
+			quoteInfo.ShipmentDropoffService = DropoffTypes.None;
+			quoteInfo.ShipmentPickupService = PickupTypes.None;
+			quoteInfo.IsInsidePickup = false;
+			quoteInfo.IsExtremeLength = false;
+			quoteInfo.ExtremeLength = 0;
+			quoteInfo.ExtremeLengthBundleCount = 0;
+			quoteInfo.IsTradeShow = false;
+			quoteInfo.IsSortnSegregate = false;
+			quoteInfo.isInsuranceRequired = false;
+			quoteInfo.isNewItem = false;
+			quoteInfo.RequiredInsuranceAmount = 0;
+			quoteInfo.ShipmentExtraService = ExtraServices.None;
+			quoteInfo.NonCommercialDeliveryType = NonCommercialDeliveryTypes.None;
+			quoteInfo.IsProtectFromFreeze = false;
+
+			// Quote Items Info.
+			quoteItemsInfo = new QuoteItemsInfo[1];
+			quoteItemsInfo[0] = new QuoteItemsInfo();
+			quoteItemsInfo[0].FAKClass = 50;
+			quoteItemsInfo[0].Hazardous = false;
+			quoteItemsInfo[0].ItemHeight = 0;
+			quoteItemsInfo[0].ItemLength = 0;
+			quoteItemsInfo[0].ItemNmfc = "85";
+			quoteItemsInfo[0].ItemWeight = 500;
+			quoteItemsInfo[0].ItemWidth = 0;
+			quoteItemsInfo[0].PackageTypeId = 0;
+			quoteItemsInfo[0].PackageTypeName = "Pallets(40x48)";
+			quoteItemsInfo[0].PalletCount = 0;
+			quoteItemsInfo[0].PieceDescription = "Boxes";
+			quoteItemsInfo[0].Pieces = 1;
+			quoteItemsInfo[0].ProductId = 1;
+		}
+
+		/// <summary>
+		/// Handles the Click event of the btnConfirmShipment control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+		private void btnConfirmShipment_Click(object sender, EventArgs e)
+		{
+			if (string.IsNullOrEmpty(txtIndex.Text))
+			{
+				MessageBox.Show("Carrier Index is Empty.");
+				txtIndex.Focus();
+			}
+			else
+			{
+				RateQuoteWebServiceReference.Service rateQuoteWebService = new RateQuoteWebServiceReference.Service();
+
+				Int32 productsCount;
+				productsCount = 1;
+
+				QuoteItemsInfo[] quoteItemsInfo = new QuoteItemsInfo[productsCount];
+				quoteItemsInfo = InsertIntoQuoteItems(productsCount, quoteItemsInfo);
+
+				ConfirmQuoteRequestInfo confirmation = new ConfirmQuoteRequestInfo();
+				confirmation.AgreementAcceptance = true;
+				confirmation.DeliveryRemarks = "test remark";
+				confirmation.PickUpRemarks = "test remark";
+				confirmation.PONo = "1000000";
+				confirmation.QuoteItems = quoteItemsInfo;
+				string RedayTime = string.Concat("07:00 PM");
+				string CloseTime = string.Concat("09:00 PM");
+				confirmation.ShipmentCloseTime = CloseTime;
+				confirmation.ShipmentReadyTime = RedayTime;
+				confirmation.ShipmentDate = DateTime.Now.AddDays(1);
+
+				int index = Convert.ToInt32(txtIndex.Text);
+				confirmation.SelectedCarrierCodeForShipment = dgvRateMyShipment.Rows[index].Cells[1].Value.ToString();
+				confirmation.ServiceTransactionId = dgvRateMyShipment.Rows[index].Cells["ServiceTransactionId"].Value.ToString();
+				confirmation.RefrenceNumber = "";
+				confirmation.CustomerBOL = "";
+				confirmation.ConsigneeAddress = new ShipmentAddress();
+				confirmation.ConsigneeAddress.CompanyName = "Asik comp 1";
+				confirmation.ConsigneeAddress.ContactPerson = "Asik AMjad 1";
+				confirmation.ConsigneeAddress.Street = "1st street";
+				confirmation.ConsigneeAddress.Fax = "";
+				confirmation.ConsigneeAddress.Phone = "1234567890";
+				confirmation.ConsigneeAddress.PhoneExtention = "1234";
+				confirmation.ConsigneeAddress.City = "Auburn";
+				confirmation.ConsigneeAddress.State = "WA";
+				confirmation.ConsigneeAddress.ZipCode = "980001";
+				confirmation.ConsigneeAddress.Country = "USA";
+
+				confirmation.ShipperAddress = new ShipmentAddress();
+				confirmation.ShipperAddress.CompanyName = "Asik Comp 2";
+				confirmation.ShipperAddress.ContactPerson = "Asik AMjad 2";
+				confirmation.ShipperAddress.Street = "2nd street";
+				confirmation.ShipperAddress.Fax = "1111111111";
+				confirmation.ShipperAddress.Phone = "2222222222";
+				confirmation.ConsigneeAddress.PhoneExtention = "1234";
+				confirmation.ShipperAddress.City = "Los Angeles";
+				confirmation.ShipperAddress.State = "CA";
+				confirmation.ShipperAddress.ZipCode = "10001";
+				confirmation.ShipperAddress.Country = "USA";
+
+				rateQuoteWebService.ConfirmShipmentCompleted += new ConfirmShipmentCompletedEventHandler(ser_ConfirmShipmentCompleted);
+
+				textBoxBolNo.Text = Convert.ToString(rateQuoteWebService.ConfirmShipment(confirmation));
+			}
+		}
+
+		/// <summary>
+		/// Inserts the into quote items.
+		/// </summary>
+		/// <param name="productsCount">The products count.</param>
+		/// <param name="quoteItemsInfo">The quote items information.</param>
+		/// <returns></returns>
+		public QuoteItemsInfo[] InsertIntoQuoteItems(Int32 productsCount, QuoteItemsInfo[] quoteItemsInfo)
+		{
+			quoteItemsInfo[0] = new QuoteItemsInfo();
+
+			quoteItemsInfo[0].FAKClass = 50;
+			quoteItemsInfo[0].Hazardous = false;
+
+			quoteItemsInfo[0].ItemHeight = 0;
+			quoteItemsInfo[0].ItemWidth = 0;
+			quoteItemsInfo[0].ItemLength = 0;
+
+			quoteItemsInfo[0].ItemNmfc = "85";
+			quoteItemsInfo[0].ItemWeight = 500;
+
+			quoteItemsInfo[0].PackageTypeId = 0;
+			quoteItemsInfo[0].PackageTypeName = "";
+			quoteItemsInfo[0].ProductId = 1;
+
+			quoteItemsInfo[0].Pieces = 1;
+			quoteItemsInfo[0].PieceDescription = "Boxes";
+			return quoteItemsInfo;
+		}
+
+		/// <summary>
+		/// Handles the ConfirmShipmentCompleted event of the ser control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="ConfirmShipmentCompletedEventArgs"/> instance containing the event data.</param>
+		void ser_ConfirmShipmentCompleted(object sender, ConfirmShipmentCompletedEventArgs e)
+		{
+			if (e.Error == null)
+			{
+				string bolNo = Convert.ToString(e.Result);
+				textBoxBolNo.Text = bolNo.ToString();
+
+			}
+			else
+			{
+				MessageBox.Show(e.Error.Message);
+			}
+		}
+
+		/// <summary>
+		/// Handles the Click event of the btnMyShipmentTrack control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+		private void btnMyShipmentTrack_Click(object sender, EventArgs e)
+		{
+			if (string.IsNullOrEmpty(textBoxBolNo.Text))
+			{
+				MessageBox.Show("No Shipment available.");
+			}
+			else
+			{
+				RateQuoteWebServiceReference.Service rateQuoteWebService = new RateQuoteWebServiceReference.Service();
+
+				TrackingInfo trackingInfo = new TrackingInfo();
+				trackingInfo = rateQuoteWebService.GetMyShipmentTrack(Username, Password, textBoxBolNo.Text, "True");
+
+				DataTable trackingDatatable = new System.Data.DataTable();
+
+				foreach (PropertyInfo info in typeof(TrackingInfo).GetProperties())
+				{
+					trackingDatatable.Columns.Add(info.Name, info.PropertyType);
+				}
+
+				trackingDatatable.AcceptChanges();
+				DataRow trackingRow = trackingDatatable.NewRow();
+
+				foreach (var property in trackingInfo.GetType().GetProperties())
+				{
+					trackingRow[property.Name] = property.GetValue(trackingInfo, null);
+				}
+
+				trackingDatatable.Rows.Add(trackingRow);
+				trackingDatatable.AcceptChanges();
+
+				dgvShipmentTrack.DataSource = trackingDatatable;
+			}
+		}
+	}
+}
